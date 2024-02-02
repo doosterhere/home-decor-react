18c38fdc3c1b90dfb541eacd729a321753ee55c8
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-02 15:02:00 +0300

    Fix of the Icon component
    *Path corrections in the xlinkHref attribute of <use> elements;
    *Code optimization for useEffect: both returns from the 'if' blocks have
    been combined into one return with two 'if' blocks;

--- src/app/components/Icon/Icon.tsx
+++ src/app/components/Icon/Icon.tsx
@@ -16,68 +16,71 @@ type IconType = {
     needParentHover?: boolean;
 };
 
-export const Icon: FC<IconType> = ({
-                                name,
-                                width,
-                                height,
-                                needHover,
-                                needParentHover
-                            }) => {
-    const ref = useRef<SVGSVGElement>(null);
-    const [hovered, setHovered] = useState(false);
+export const Icon: FC<IconType> =
+    ({
+         name,
+         width,
+         height,
+         needHover,
+         needParentHover
+     }) => {
+        const ref = useRef<SVGSVGElement>(null);
+        const [hovered, setHovered] = useState(false);
 
-    const mouseIn = () => {
-        setHovered(true);
-    }
-
-    const mouseOut = () => {
-        setHovered(false);
-    }
-
-    useEffect(() => {
-        const svg = ref.current;
-
-        if (needHover) {
-            svg?.addEventListener('mouseenter', mouseIn);
-            svg?.addEventListener('mouseleave', mouseOut);
-
-            return () => {
-                svg?.removeEventListener('mouseenter', mouseIn);
-                svg?.removeEventListener('mouseleave', mouseOut);
-            };
+        const mouseIn = () => {
+            setHovered(true);
         }
 
-        if (needParentHover) {
-            svg?.parentNode?.addEventListener('mouseenter', mouseIn);
-            svg?.parentNode?.addEventListener('mouseleave', mouseOut);
+        const mouseOut = () => {
+            setHovered(false);
+        }
+
+        useEffect(() => {
+            const svg = ref.current;
+
+            if (needHover) {
+                svg?.addEventListener('mouseenter', mouseIn);
+                svg?.addEventListener('mouseleave', mouseOut);
+            }
+
+            if (needParentHover) {
+                svg?.parentNode?.addEventListener('mouseenter', mouseIn);
+                svg?.parentNode?.addEventListener('mouseleave', mouseOut);
+            }
 
             return () => {
-                svg?.parentNode?.removeEventListener('mouseenter', mouseIn);
-                svg?.parentNode?.removeEventListener('mouseleave', mouseOut);
-            };
-        }
-    }, [needHover, needParentHover]);
+                if (needHover) {
+                    svg?.removeEventListener('mouseenter', mouseIn);
+                    svg?.removeEventListener('mouseleave', mouseOut);
+                }
 
-    if (!needHover && !needParentHover) {
+                if (needParentHover) {
+                    svg?.parentNode?.removeEventListener('mouseenter', mouseIn);
+                    svg?.parentNode?.removeEventListener('mouseleave', mouseOut);
+                }
+            }
+        }, [needHover, needParentHover]);
+
+        if (!needHover && !needParentHover) {
+            return (
+                <svg className={`icon icon-${name}`} style={{width, height}} ref={ref}>
+                    <use
+                        xlinkHref={`/images/sprite.svg#${name}`}
+                    ></use>
+                </svg>
+            );
+        }
+
         return (
             <svg className={`icon icon-${name}`} style={{width, height}} ref={ref}>
                 <use
-                    xlinkHref={`./images/sprite.svg#${name}`}
-                ></use>
+                    style={{display: hovered ? 'none' : 'block'}}
+                    xlinkHref={`/images/sprite.svg#${name}`}
+                />
+                <use
+                    style={{display: hovered ? 'block' : 'none'}}
+                    xlinkHref={`/images/sprite.svg#${name}-hovered`}
+                />
             </svg>
         );
-    }
-
-    return (
-        <svg className={`icon icon-${name}`} style={{width, height}} ref={ref}>
-            <use
-                style={{display: hovered ? 'none' : 'block'}}
-                xlinkHref={`./images/sprite.svg#${name}`}
-            />
-            <use
-                style={{display: hovered ? 'block' : 'none'}}
-                xlinkHref={`./images/sprite.svg#${name}-hovered`}
-            />
-        </svg>
-    );
-};
+    };
