42f3aa26cb8159a067c840176d9b74ab3330f9a8
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-05 13:25:13 +0300

    Changing the AppRouter component to use lazy loading

--- src/app/components/AppRouter.tsx
+++ src/app/components/AppRouter.tsx
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, {lazy} from 'react';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
 import {selectIsLogged} from "../store";
 import {ROUTES} from "../constants";
 import {useAppSelector} from "../hooks";
 
-import {Footer, Header} from "../components";
-import Main from "../pages/Main/Main";
-import Catalog from "../pages/Catalog/Catalog";
-import Detail from "../pages/Catalog/Detail/Detail";
-import Login from "../pages/User/Login/Login";
-import Terms from "../pages/User/Terms/Terms";
-import PageNotFound from "../pages/PageNotFound/PageNotFound";
+import {Layout} from "../components";
 
+const Main = lazy(() => import('../pages/Main/Main'));
+const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
+const Detail = lazy(() => import('../pages/Catalog/Detail/Detail'));
+const Login = lazy(() => import('../pages/User/Login/Login'));
+const Terms = lazy(() => import('../pages/User/Terms/Terms'));
+const PageNotFound = lazy(() => import('../pages/PageNotFound/PageNotFound'));
 
 export const AppRouter = () => {
     const isLogged = useAppSelector(selectIsLogged);
 
     return (
         <BrowserRouter>
-            <Header/>
             <Routes>
-                <Route path={ROUTES.HOME} element={<Main/>}/>
-                <Route path={ROUTES.CATALOG} element={<Catalog/>}/>
-                <Route path={`${ROUTES.PRODUCT}/:url`} element={<Detail/>}/>
-                <Route path={ROUTES.SIGNUP} element={isLogged
-                    ? <Navigate replace to={ROUTES.HOME}/>
-                    : <Login/>}
-                />
-                <Route path={ROUTES.LOGIN} element={isLogged
-                    ? <Navigate replace to={ROUTES.HOME}/>
-                    : <Login/>}
-                />
-                <Route path={ROUTES.TERMS} element={<Terms/>}/>
-                <Route path='*' element={<PageNotFound/>}/>
+                <Route path={ROUTES.HOME} element={<Layout/>}>
+                    <Route index element={<Main/>}/>
+                    <Route path={ROUTES.CATALOG} element={<Catalog/>}/>
+                    <Route path={`${ROUTES.PRODUCT}/:url`} element={<Detail/>}/>
+                    <Route path={ROUTES.SIGNUP}
+                           element={isLogged
+                               ? <Navigate to={ROUTES.HOME}/>
+                               : <Login/>
+                           }
+                    />
+                    <Route path={ROUTES.LOGIN}
+                           element={isLogged
+                               ? <Navigate to={ROUTES.HOME}/>
+                               : <Login/>
+                           }
+                    />
+                    <Route path={ROUTES.TERMS} element={<Terms/>}/>
+                    <Route path='*' element={<PageNotFound/>}/>
+                </Route>
             </Routes>
-            <Footer/>
         </BrowserRouter>
     );
 }
