cde9afc313299dadd1b10ddb76cbddbc892df173
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-07 12:05:24 +0300

    Changing the ProductCard component
    [div.product-card__info] and [div.product-card__extra] have been placed
    in one empty tag because they are displayed with the same logical
    condition.

--- src/app/components/ProductCard/ProductCard.tsx
+++ src/app/components/ProductCard/ProductCard.tsx
@@ -110,30 +110,30 @@ export const ProductCard: FC<IProductCardProps> =
                          style={{backgroundImage: `url(${SERVER_STATIC_PATH + product.image})`}}></div>
                     <div className='product-card__name'>{product.name}</div>
                     {!isLight &&
-                        <div className='product-card__info'>
-                            <div className='product-card__price'>{product.price} BYN</div>
-                            <div className='product-card__action'>
-                                {countInCart === 0 &&
-                                    <button className='button' onClick={addToCart}>В корзину</button>
-                                }
-                                {countInCart > 0 &&
-                                    <button className='button button_transparent button_in-cart'
-                                            onClick={removeFromCart}>
-                                        <span>В корзине</span>
-                                        <span>Удалить</span>
-                                    </button>
-                                }
+                        <>
+                            <div className='product-card__info'>
+                                <div className='product-card__price'>{product.price} BYN</div>
+                                <div className='product-card__action'>
+                                    {countInCart === 0 &&
+                                        <button className='button' onClick={addToCart}>В корзину</button>
+                                    }
+                                    {countInCart > 0 &&
+                                        <button className='button button_transparent button_in-cart'
+                                                onClick={removeFromCart}>
+                                            <span>В корзине</span>
+                                            <span>Удалить</span>
+                                        </button>
+                                    }
+                                </div>
                             </div>
-                        </div>
-                    }
-                    {!isLight &&
-                        <div className='product-card__extra'>
-                            <CountSelector count={count} updateCount={updateCount}/>
-                            <Link to={`${ROUTES.PRODUCT}/${product.url}`} className='product-card__detail'>
-                                <Icon name={IconName.dots} needParentHover/>
-                                <span>Подробнее</span>
-                            </Link>
-                        </div>
+                            <div className='product-card__extra'>
+                                <CountSelector count={count} updateCount={updateCount}/>
+                                <Link to={`${ROUTES.PRODUCT}/${product.url}`} className='product-card__detail'>
+                                    <Icon name={IconName.dots} needParentHover/>
+                                    <span>Подробнее</span>
+                                </Link>
+                            </div>
+                        </>
                     }
                 </div>
             );
