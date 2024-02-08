b6edf27a44bb1eed6f9e4862cc4d4e5285670ce4
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-08 11:37:30 +0300

    Adding a new API: cartAPI
    The API contains three endpoints: the getCart query, the getCartCount
    query and the updateCart mutation.

---
+++ src/app/store/api/cartApi.ts
@@ -0,0 +1,39 @@
+import {createApi} from "@reduxjs/toolkit/query/react";
+
+import {baseQueryWithReAuth} from "../../utils";
+
+import {CartType, DefaultResponseType, ICartItem} from "../../types";
+
+export const cartAPI = createApi({
+    reducerPath: 'cartAPI',
+    tagTypes: ['cart'],
+    baseQuery: baseQueryWithReAuth,
+    endpoints: (builder) => ({
+        getCart: builder.query<CartType | DefaultResponseType, void>({
+            query: () => ({
+                headers: {
+                    withCredentials: 'true'
+                },
+                url: 'cart'
+            }),
+            providesTags: ['cart']
+        }),
+        updateCart: builder.mutation<CartType | DefaultResponseType, ICartItem>({
+            query: (patch) => ({
+                url: 'cart',
+                method: 'POST',
+                body: patch
+            }),
+            invalidatesTags: ['cart']
+        }),
+        getCartCount: builder.query<number | DefaultResponseType, void>({
+            query: () => ({
+                headers: {
+                    withCredentials: 'true'
+                },
+                url: 'cart/count'
+            }),
+            providesTags: ['cart']
+        })
+    })
+});
