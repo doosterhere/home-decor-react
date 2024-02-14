5795babc68f88ee614f9435ff66d345f88826851
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-14 14:22:14 +0300

    Changing the useCartRefetch hook
    The declaration of the getCart function has been removed and exported
    fetchCart util is now used instead. Also, the hook now returns the
    ICartItem array returned from the useCart hook, for more convenient use
    in components that will use the ProductCard component as a child.

--- src/app/hooks/useCartRefetch.ts
+++ src/app/hooks/useCartRefetch.ts
@@ -1,41 +1,22 @@
 import {useEffect} from "react";
 
-import {cartAPI, enqueueErrorMessage, resetNeedCartRefetch, selectIsLogged, selectNeedRefetch, setCart} from "../store";
-import {useAppDispatch, useAppSelector} from "./redux";
+import {resetNeedCartRefetch, selectCart, selectIsLogged, selectNeedRefetch} from "../store";
+import {useAppDispatch, useAppSelector, useCart} from "../hooks";
+import {fetchCart} from "../utils";
 
 export function useCartRefetch() {
     const dispatcher = useAppDispatch();
     const isLogged = useAppSelector(selectIsLogged);
     const needRefetch = useAppSelector(selectNeedRefetch);
+    const cart = useAppSelector(selectCart);
 
     useEffect(() => {
-        getCart().catch(error => console.log('error in fetch after login/logout: ', error));
+        fetchCart(dispatcher).catch(error => console.log('error in fetch after login/logout: ', error));
 
         if (needRefetch) {
             dispatcher(resetNeedCartRefetch());
         }
     }, [isLogged, needRefetch]);
 
-    async function getCart() {
-        const cartPromise = dispatcher(cartAPI.endpoints?.getCart.initiate());
-        const refetchCartPromise = cartPromise.refetch.bind(cartPromise);
-
-        await refetchCartPromise()
-            .then(res => {
-                if (res && res.data && 'items' in res.data) {
-                    dispatcher(setCart(res.data));
-
-                    return;
-                }
-
-                throw new Error();
-            })
-            .catch(() => {
-                    dispatcher(enqueueErrorMessage('Не удалось загрузить корзину товаров, попробуйте позже'));
-                }
-            )
-            .finally(() => {
-                cartPromise.unsubscribe();
-            });
-    }
+    return useCart(cart);
 }
