62d66e51b86b03c703e28cc2202c3eff52fd889d
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-14 11:19:51 +0300

    Changing the cartReducer
    The interface for the initial state of the cartReducer has been added
    and the initial state has been changed. Three new actions have been
    added: setCart, setNeedCartRefetch and resetNeedCartRefetch. Two new
    selectors have been added: selectCart, selectNeedRefetch.

--- src/app/store/reducers/cartReducer.ts
+++ src/app/store/reducers/cartReducer.ts
@@ -1,19 +1,41 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {CartType} from "../../types";
+
+interface ICartState {
+    count: number;
+    cart: CartType;
+    needRefetch: boolean;
+}
+
+const initialState: ICartState = {
+    count: 0,
+    cart: {items: []},
+    needRefetch: false
+}
 
 export const cartReducer = createSlice({
     name: 'cart',
-    initialState: {
-        count: 0
-    },
+    initialState,
     reducers: (create) => ({
         setCartCount: create.reducer((state, action: PayloadAction<number>) => {
             state.count = action.payload;
+        }),
+        setCart: create.reducer((state, action: PayloadAction<CartType>) => {
+            state.cart = action.payload;
+        }),
+        setNeedCartRefetch: create.reducer((state) => {
+            state.needRefetch = true;
+        }),
+        resetNeedCartRefetch: create.reducer((state) => {
+            state.needRefetch = false;
         })
     }),
     selectors: {
-        selectCartCount: (state) => state.count
+        selectCartCount: (state) => state.count,
+        selectCart: (state) => state.cart,
+        selectNeedRefetch: (state) => state.needRefetch
     }
 });
 
-export const {setCartCount} = cartReducer.actions;
-export const {selectCartCount} = cartReducer.selectors;
+export const {setCartCount, setCart, setNeedCartRefetch, resetNeedCartRefetch} = cartReducer.actions;
+export const {selectCartCount, selectCart, selectNeedRefetch} = cartReducer.selectors;
