69418f784fb6a97bd2b3debca9bdecf6bebf90fd
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-15 16:24:15 +0300

    Changing the CartProduct component
    The ability of removing a product from the cart has been implemented.

--- src/app/pages/Order/Cart/CartProduct.tsx
+++ src/app/pages/Order/Cart/CartProduct.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useState} from 'react';
 
 import {SERVER_STATIC_PATH} from "../../../constants";
+import {useUpdateCountOfProductInCart} from "../../../hooks";
 
 import {CartProductType, IconName} from "../../../types";
 
@@ -13,9 +14,11 @@ interface ICartProduct {
 
 const CartProduct: FC<ICartProduct> = ({product, quantity}) => {
     const [count, setCount] = useState(quantity);
+    const updateCart = useUpdateCountOfProductInCart();
 
-    const removeFromCart = () => {
-    }
+    const handleRemoveFromCart = async () => {
+        await updateCart(product?.id, 0);
+    };
 
     return (
         <div className="cart__product">
@@ -24,7 +27,7 @@ const CartProduct: FC<ICartProduct> = ({product, quantity}) => {
             <div className="cart__product-name">{product.name}</div>
             <CountSelector count={count} updateCount={setCount}/>
             <div className="cart__product-price">{product.price} BYN</div>
-            <div className="cart__product-remove" onClick={removeFromCart}>
+            <div className="cart__product-remove" onClick={handleRemoveFromCart}>
                 <Icon name={IconName.closeCross} needHover/>
             </div>
         </div>
