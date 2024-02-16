04aa9b89c9326ed4d1e239e079605003b9181ad8
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-16 15:10:41 +0300

    Changing the Detail component
    Now the useCartRefetch hook is used to sync the state of the cart when
    the component is loaded.

--- src/app/pages/Catalog/Detail/Detail.tsx
+++ src/app/pages/Catalog/Detail/Detail.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useParams} from "react-router-dom";
 
 import './Detail.scss';
 
-import {productAPI} from "../../../store";
+import {productAPI, setCartCount} from "../../../store";
 import {SERVER_STATIC_PATH} from "../../../constants";
-import {useScrollToAnchor} from "../../../hooks";
+import {useAppDispatch, useCartRefetch, useScrollToAnchor} from "../../../hooks";
 
 import DetailRecommendedProducts from "./DetailRecommendedProducts";
 import DetailInfo from "./DetailInfo";
@@ -13,6 +13,12 @@ import DetailInfo from "./DetailInfo";
 const Detail = () => {
     const params = useParams();
     const {data: product} = productAPI.useGetProductQuery(params['url'] as string);
+    const dispatcher = useAppDispatch();
+    const cart = useCartRefetch();
+
+    useEffect(() => {
+        dispatcher(setCartCount(cart.itemsCount));
+    }, [cart, dispatcher]);
 
     useScrollToAnchor();
 
