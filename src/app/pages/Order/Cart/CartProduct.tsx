405fe8bc67963dbed8ca7106fec5077a09519b59
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-02-24 14:19:03 +0300

    Changing the CartProduct component
    * The component now receives only one prop - the 'product'. The quantity
      of this product is taken using the 'useGetCountInCart' hook.
    * The use of the debounced counter has been replaced by the use of the
      debounced function.
    * After updating the cart of a logged in user or an anonymous user, the
      flag 'newCartHasBeenReceived' is reset.

--- src/app/pages/Order/Cart/CartProduct.tsx
+++ src/app/pages/Order/Cart/CartProduct.tsx
@@ -1,7 +1,14 @@
 import React, {FC, useEffect, useState} from 'react';
 
 import {SERVER_STATIC_PATH} from "../../../constants";
-import {useDebounceValue, useUpdateCountOfProductInCart} from "../../../hooks";
+import {resetNewCartHasBeenReceived, selectNewCartHasBeenReceived} from "../../../store";
+import {
+    useAppDispatch,
+    useAppSelector,
+    useDebounceFunction,
+    useGetCountInCart,
+    useUpdateCountOfProductInCart
+} from "../../../hooks";
 
 import {CartProductType, IconName} from "../../../types";
 
@@ -9,33 +16,44 @@ import {CountSelector, Icon} from "../../../components";
 
 interface ICartProduct {
     product: CartProductType;
-    quantity: number;
 }
 
-const CartProduct: FC<ICartProduct> = ({product, quantity}) => {
-    const [count, setCount] = useState(quantity);
-    const debouncedCount = useDebounceValue(count, 500);
-    const [previousDebouncedCount, setPreviousDebouncedCount] = useState(debouncedCount);
+const CartProduct: FC<ICartProduct> = ({product}) => {
+    const [countInCart] = useGetCountInCart(product?.id);
+    const [count, setCount] = useState(countInCart || 1);
     const updateCart = useUpdateCountOfProductInCart();
+    const debouncedUpdateCart = useDebounceFunction(updateCountInCart, 500);
+    const hasNewCartBeenReceivedAfterUserChange = useAppSelector(selectNewCartHasBeenReceived);
+    const dispatcher = useAppDispatch();
 
     useEffect(() => {
-        if (debouncedCount !== previousDebouncedCount) {
-            void updateCart(product?.id, debouncedCount);
+        if (countInCart !== count && hasNewCartBeenReceivedAfterUserChange) {
+            setCount(countInCart || 1);
+            dispatcher(resetNewCartHasBeenReceived());
         }
-
-        setPreviousDebouncedCount(debouncedCount);
-    }, [debouncedCount]);
+    }, [countInCart, hasNewCartBeenReceivedAfterUserChange]);
 
     const handleRemoveFromCart = async () => {
         await updateCart(product?.id, 0);
     };
+
+    function updateCountInCart(count: number) {
+        if (countInCart) {
+            void updateCart(product?.id, count);
+        }
+    }
+
+    const updateCount = (count: number) => {
+        setCount(count);
+        debouncedUpdateCart(count)
+    }
 
     return (
         <div className="cart__product">
             <div className="cart__product-image"
                  style={{backgroundImage: `url(${SERVER_STATIC_PATH + product.image})`}}></div>
             <div className="cart__product-name">{product.name}</div>
-            <CountSelector count={count} updateCount={setCount}/>
+            <CountSelector count={count} updateCount={updateCount}/>
             <div className="cart__product-price">{product.price} BYN</div>
             <div className="cart__product-remove" onClick={handleRemoveFromCart}>
                 <Icon name={IconName.closeCross} needHover/>
