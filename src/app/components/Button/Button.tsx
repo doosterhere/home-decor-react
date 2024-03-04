b53fb0b745df6a12313b463e6e013bcc2faba854
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-03-04 17:03:38 +0300

    Adding a new Button component
    This is a memoized, reusable component with the ability to display
    loading state for asynchronous operations. Depending on the presence of
    the href attribute, the component returns an <a> or <button> element.

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,13 +1,69 @@
-import React, {FC, PropsWithChildren} from 'react';
+import React, {
+    AnchorHTMLAttributes,
+    ButtonHTMLAttributes,
+    FC,
+    KeyboardEvent,
+    memo,
+    MouseEvent,
+    PropsWithChildren
+} from "react";
 
-const Button: FC<PropsWithChildren> = ({children, ...attributes}) => {
-    return (
-        <div>
-            <button {...attributes}>
-                {children}
+import classNames from "classnames";
+import CircularProgress from "@mui/material/CircularProgress";
+
+import './Button.scss';
+
+interface IButtonProps extends PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement> & AnchorHTMLAttributes<HTMLAnchorElement>> {
+    onClick?: (event: MouseEvent<HTMLButtonElement>
+        | KeyboardEvent<HTMLButtonElement>
+        | MouseEvent<HTMLAnchorElement>) => void;
+    className?: string;
+    disabled?: boolean;
+    isLoading?: boolean;
+}
+
+export const Button: FC<IButtonProps> = memo(
+    ({
+         children = 'Default button',
+         onClick,
+         className = '',
+         disabled = false,
+         isLoading = false,
+         ...attributes
+     }) => {
+        const classes = classNames('button', className);
+
+        const handle = (event: MouseEvent<HTMLButtonElement>
+            | KeyboardEvent<HTMLButtonElement>
+            | MouseEvent<HTMLAnchorElement>) => {
+            if (!isLoading && onClick) {
+                onClick(event);
+            }
+        };
+
+        const renderButton = (
+            <button
+                className={classes}
+                disabled={disabled}
+                onClick={handle}
+                {...attributes}
+            >
+                {isLoading
+                    ? <CircularProgress color="inherit" size={24}/>
+                    : children
+                }
             </button>
-        </div>
-    );
-};
+        );
 
-export default Button;
+        const renderAnchor = (
+            <a
+                className={classes}
+                onClick={handle}
+                {...attributes}
+            >
+                {children}
+            </a>
+        );
+
+        return attributes.href ? renderAnchor : renderButton;
+    });
