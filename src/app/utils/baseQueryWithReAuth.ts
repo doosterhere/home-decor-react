972afd41914a812df0315f8a6292ef1c77b2b521
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-04-08 20:26:15 +0300

    Changing the baseQueryWithReAuth utility
    Functionality to prevent multiple unauthorized errors after access token
    expiration has been implemented using async-mutex.

--- src/app/utils/baseQueryWithReAuth.ts
+++ src/app/utils/baseQueryWithReAuth.ts
@@ -1,5 +1,6 @@
 import type {BaseQueryApi, BaseQueryFn, FetchArgs, FetchBaseQueryError,} from '@reduxjs/toolkit/query';
 import {fetchBaseQuery} from '@reduxjs/toolkit/query';
+import {Mutex} from 'async-mutex';
 
 import {
     RootState,
@@ -14,6 +15,7 @@ import {BASE_API} from "../constants";
 
 import {DefaultResponseType, LoginResponseType} from "../types";
 
+const mutex = new Mutex();
 const baseQueryWithAuthHeaders = fetchBaseQuery({
     baseUrl: BASE_API,
     credentials: 'include',
@@ -38,6 +40,7 @@ export const baseQueryWithReAuth: BaseQueryFn<
     unknown,
     FetchBaseQueryError
 > = async (args, api, extraOptions) => {
+    await mutex.waitForUnlock();
     let result = await baseQueryWithAuthHeaders(args, api, extraOptions);
 
     if (result.error &&
@@ -46,29 +49,41 @@ export const baseQueryWithReAuth: BaseQueryFn<
             result.error.status === 500 && (result.error.data as DefaultResponseType).message === 'jwt expired'
         )
     ) {
-        const state = api.getState() as RootState;
-        const refreshToken = state.auth.refreshToken;
-        const refreshResult = await baseQueryWithAuthHeaders({
-                url: 'refresh',
-                method: 'POST',
-                body: {refreshToken}
-            },
-            api,
-            extraOptions);
+        if (!mutex.isLocked()) {
+            const state = api.getState() as RootState;
+            const refreshToken = state.auth.refreshToken;
 
-        if (refreshResult.error && refreshResult.error.status === 400) {
-            logout(api);
-            return refreshResult;
-        }
+            try {
+                const refreshResult = await baseQueryWithAuthHeaders({
+                        url: 'refresh',
+                        method: 'POST',
+                        body: {refreshToken}
+                    },
+                    api,
+                    extraOptions);
 
-        if (refreshResult.data && (refreshResult.data as LoginResponseType).userId) {
-            const userData = refreshResult.data as LoginResponseType;
-            api.dispatch(setAccessToken(userData.accessToken));
-            api.dispatch(setRefreshToken(userData.refreshToken));
-            result = await baseQueryWithAuthHeaders(args, api, extraOptions);
+                if (refreshResult.error && refreshResult.error.status === 400) {
+                    logout(api);
+                    return refreshResult;
+                }
+
+                if (refreshResult.data && (refreshResult.data as LoginResponseType).userId) {
+                    const userData = refreshResult.data as LoginResponseType;
+                    api.dispatch(setAccessToken(userData.accessToken));
+                    api.dispatch(setRefreshToken(userData.refreshToken));
+                    result = await baseQueryWithAuthHeaders(args, api, extraOptions);
+                } else {
+                    logout(api);
+                }
+            } finally {
+                mutex.release();
+            }
         } else {
-            logout(api);
+            await mutex.waitForUnlock();
+            result = await baseQueryWithAuthHeaders(args, api, extraOptions);
         }
     }
+
     return result;
 };
+
