8c4d67d603ca5de27bc2ad66a686a726c06757b4
Mikalai Shcharbakou <doosterhere@gmail.com>
2024-05-22 13:36:50 +0300

    Changing the makeTypesQueryString util
    Now the function returns an empty string if it receives an empty array.

--- src/app/utils/makeTypesQueryString.ts
+++ src/app/utils/makeTypesQueryString.ts
@@ -1,5 +1,8 @@
 import {TypeType} from "../types";
 
 export const makeTypesQueryString = (paramsArray: Partial<TypeType>[]): string => {
+    if (!paramsArray.length) {
+        return '';
+    }
     return '?' + paramsArray.map(param => 'types=' + param.url).join('&');
 }
